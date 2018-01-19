ccdd05b7a047aed101f3421aeafa421c948b0e6a
umarkotak <umarkotak@yahoo.co.id>
2018-01-19 09:40:01 +0700

    add routing with action

--- content.php
+++ content.php
@@ -4,7 +4,15 @@ if (isset($_GET['page'])) {
 
   if ($page == 'home')                                      { include "pages/home.php"; }
   if ($page == 'setting')                                   { include "pages/setting.php"; }
+
+} elseif (isset($_GET['action'])) {
+  $action = $_GET['action'];
+
+
+
 } else {
+
   include "pages/home.php";
+
 }
 ?>
